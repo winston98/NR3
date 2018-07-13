c4188c7ed31e88d2a7ad398e4e85a989a1f4f021
Flamingo <wli@cs.hmc.edu>
2018-07-12 17:46:29 -0700

    Update functionalities: multi-sample file, output json files

---
+++ main.py
@@ -0,0 +1,53 @@
+import csv
+from sample import Sample
+from tkinter import *
+
+master = Tk()
+master.title("CSV Parser")
+Label(master, text="Enter file name: ").pack()
+f_input = Entry(master)
+f_input.pack()
+f_input.focus_set()
+Label(master, text="Enter tons today: ").pack()
+t_input = Entry(master)
+t_input.pack()
+t_input.focus_set()
+
+def callback():
+    filename = f_input.get()
+    try:
+        file = open(filename + ".csv")
+    except (OSError, IOError):
+        error = Toplevel(master)
+        error.title("Error")
+        Label(error, text="Invalid filename").pack()   
+    with file:
+        reader = csv.reader(file)
+        sample = Sample()
+        for row in reader:
+            if row[1] == "Sample Id": # skip header row
+                continue
+            elif sample.getName() != row[1]:
+                if sample.getName() != None:
+                    sample.totalCarbon()
+                    sample.makeFile()
+                    sample = Sample()
+                sample.add("sampleName", row[1])
+                sample.add("tons/day", float(t_input.get())) # user input
+            analyte = row[12]
+            data = row[13]
+            if "<" in data:
+                data = int(float(data[1:]))
+            data = float(data)
+            if "moisture" in analyte.lower():
+                sample.add("moisture", float(row[13]))
+                continue
+            sample.makeEntry(analyte, data)
+        sample.totalCarbon()
+        sample.makeFile()
+
+Button(master, text="OK", command=callback).pack()
+Button(master, text="Quit", command=master.quit).pack()
+
+mainloop()
+
---
+++ sample.py
@@ -0,0 +1,52 @@
+import json
+
+class Sample:
+
+    def __init__(self):
+        self.output = {}
+        self.otherSolids = 0.0
+
+    def add(self, key, value):
+        self.output[key] = value
+
+    def getName(self):
+        if "sampleName" in self.output:
+            return self.output["sampleName"]
+        else:
+            return None
+
+    def makeEntry(self, analyte, data):
+        entry = {}
+        analyte = analyte.lower()
+        if "nitrogen" not in analyte and "ammonia" not in analyte:
+            data /= 10000
+        entry["dryBasis%"] = data
+        entry["asReceived%"] = data * (1 - self.output["moisture"]/100)
+        lbs = 2000 * entry["asReceived%"] / 100
+        if "potassium" in analyte: # 1:1.2046 K to K2O
+            lbs *= 1.2046
+        elif "phosphorus" in analyte: # 1:2.2914 P to P2O5
+            lbs *= 2.2914
+        self.otherSolids += lbs
+        entry["lbs/ton"] = lbs
+        entry["lbs/day"] = self.output["tons/day"] * lbs
+        entry["lbs/yr"] = 365 * entry["lbs/day"]
+        # N2O & CO2 equivalent only matters for N and NH3
+        if "nitrogen" in analyte or "ammonia" in analyte:
+            entry["lbs/dayNO2"] = 1.3634 * entry["lbs/day"]
+            entry["lbs/dayCO2"] = 298 * entry["lbs/dayNO2"]
+            entry["tons/yrCO2"] = 365 * entry["lbs/dayCO2"] / 2000
+        self.output[analyte] = entry
+
+    # Many unnecessary middle steps in case intermdiate values need displayed
+    def totalCarbon(self):
+        percentSolid = 100 - self.output["moisture"]
+        solidsPerTon = percentSolid / 100 * 2000
+        carbonPerTon = solidsPerTon - self.otherSolids
+        carbonPerDay = self.output["tons/day"] * carbonPerTon / 2000
+        co2PerDay = 3.664191096 * carbonPerDay
+        self.output["co2PerDay"] = co2PerDay
+
+    def makeFile(self):
+        with open(self.output["sampleName"] + ".json", "w") as outfile:
+            json.dump(self.output, outfile)
